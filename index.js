633fcd8da04b7484645397cf1151ad6e8ba186c5
ROFIHAT-0LATINWO <rofiatolatinwo@gmail.com>
2022-11-10 16:02:59 -0700

    keeping track of activity

---
+++ index.js
@@ -0,0 +1,24 @@
+let countDownDate = new Date("2022/12/24 12:00:00 GMT+0200").getTime();
+
+let count = setInterval(function () {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    document.querySelector("h1").innerHTML = "We are launching now... ";
+    document.getElementById("days").innerHTML = "0";
+    document.getElementById("hours").innerHTML = "0";
+    document.getElementById("minutes").innerHTML = "0";
+    document.getElementById("seconds").innerHTML = "0";
+  }
+}, 1000);
